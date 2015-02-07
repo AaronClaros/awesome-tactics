ca314258ee9b948540f62789dd65de2a7c978b9e
AaronClaros <kevin031995@gmail.com>
2015-02-06 22:07:35 -0400

    Añadiendo un generador de casillas
    Creado un gameobject vacio llamado GameController.
    Creado un script llamado Tile, que resalta con color azul la casilla
    sobre la que esta el raton.
    Creado un script llamado GridGenerator, que crea una lista de prefabs, y
    los pone en la escena.
    Creada una carpera llamada Textures.
    imagen tile añadida a Textures
    Creada una carpera llamada Materials.
    Creado un material llamado Grid, shader=Bumped Diff...
    imagen tile añadida a Grid
    Creada una carpera llamada Prefabs.
    Creado quad llamado Tile, añadido script Tile a quad, añadido material
    Grid.
    Ground movido a la posicion (0, -0.01, 0).
    
    GridGenerator añadido a GameController.

---
+++ Tactical RPG 3D/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GridGenerator : MonoBehaviour {
+	public static GridGenerator instance;
+	
+	public GameObject TilePrefab;
+
+	public int mapSideXSize = 11;
+	public int mapSideYSize = 11;
+
+	List <List<Tile>> map = new List<List<Tile>>();
+
+	void Aweke(){
+		instance = this;
+	}
+	// Use this for initialization
+	void Start () {
+		generateMap();
+	}
+
+	void generateMap() {
+		map = new List<List<Tile>>();
+		for (int i = 0; i < mapSideXSize; i++) {
+			List <Tile> row = new List<Tile>();
+			for (int j = 0; j < mapSideYSize; j++) {
+				Tile tile = ((GameObject)Instantiate(TilePrefab, new Vector3(i - Mathf.Floor(mapSideXSize/2),0, -j + Mathf.Floor(mapSideYSize/2)), Quaternion.Euler(90,0,0))).GetComponent<Tile>();
+				tile.gridPosition = new Vector2(i, j);
+				row.Add (tile);
+			}
+			map.Add(row);
+		}
+	}
+}
