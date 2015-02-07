d2e438be725d8395151f043128d22260c188ca0e
AaronClaros <kevin031995@gmail.com>
2015-02-06 21:45:52 -0400

    Añadiendo script de control de camara
    Creada carpeta llamada Scripts.
    Creado script llamado OrbitCamera que segun se mueva el raton, gira al
    rededor de un objeto, con la opcion de hacer zoom (fuente:
    http://answers.unity3d.com/questions/25965/camera-orbit-on-mouse-drag.html).
    Creado script llamado CameraController, cuando se preciona la tecla
    "espacio" el script OrbitCamera se deshabilita. y cuando se vuelve a
    precionar se habilita nuevamente.
    OrbitCamera y CameraController añadidos al objeto Main Camera.

---
+++ Tactical RPG 3D/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	private OrbitCamera oc;
+	// Use this for initialization
+	void Start () {
+		oc = GetComponent<OrbitCamera> ();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")){
+			oc.enabled=!oc.enabled;
+		}
+		
+	}
+}
